d11aba5d278c2bbfd65b197ba3affed926f73af6
Fenil Nikhil Gholani <fgholani@umd.edu>
2021-03-24 04:51:51 +0530

    searchbar in process

---
+++ app/src/main/java/com/example/healthapp/InfoActivity.kt
@@ -0,0 +1,8 @@
+package com.example.healthapp
+
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class InfoActivity : AppCompatActivity() {
+    var listView: ListView? = null
+}
