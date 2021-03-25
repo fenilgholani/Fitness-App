40e1b6327d91c822b2bd2daabbaca4f20bd2e299
Fenil Nikhil Gholani <fgholani@umd.edu>
2021-03-25 16:21:25 +0530

    Search

--- app/src/main/java/com/example/healthapp/InfoActivity.kt
+++ app/src/main/java/com/example/healthapp/InfoActivity.kt
@@ -1,8 +1,45 @@
 package com.example.healthapp
 
-import android.widget.ListView
+import android.os.Bundle
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import java.util.*
+import kotlin.collections.ArrayList
 
 class InfoActivity : AppCompatActivity() {
-    var listView: ListView? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_info)
+//
+//        val search = findViewById<SearchView>(R.id.searchView)
+//        val listView = findViewById<ListView>(R.id.listView)
+//
+//        val names = arrayOf("Push up", "Pull Up", "Sit up", "Squats")
+//
+//        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, names)
+//
+//        listView.adapter = adapter
+//
+//        search.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                search.clearFocus()
+//
+//                if(names.contains(query))
+//                    adapter.filter.filter(query)
+//                else
+//                    Toast.makeText(applicationContext,"Item not found", Toast.LENGTH_SHORT).show()
+//                return false
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                adapter.filter.filter(newText)
+//                return false
+//            }
+//        })
+
+    }
 }
