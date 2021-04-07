e8caad0e638e734f3872b41215c15a60d89a253a
TytanT <lovebite@Lovebites-MacBook-Pro.local>
2021-04-07 17:35:09 -0400

    Functioning exercise addition

---
+++ app/src/main/java/com/example/healthapp/ExerciseAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.healthapp
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ExerciseAdapter(
+    private var exerciseNames: ArrayList<String> // list of selected exercises for the day
+) :
+
+    RecyclerView.Adapter<ExerciseAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val itemTitle: TextView = itemView.findViewById(R.id.today_exercise_name)
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v: View = LayoutInflater.from(parent.context).inflate(
+            R.layout.row_todaysrow,
+            parent,
+            false
+        )
+        return  ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemTitle.text = exerciseNames[position]
+    }
+
+    override fun getItemCount(): Int {
+        return exerciseNames.size
+    }
+
+
+}
+
--- app/src/main/java/com/example/healthapp/TodayExerciseActivity.kt
+++ app/src/main/java/com/example/healthapp/TodayExerciseActivity.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class TodayExerciseActivity : AppCompatActivity() {
@@ -16,33 +19,35 @@ class TodayExerciseActivity : AppCompatActivity() {
     private var save: Button? = null
     private var add: Button? = null
     var bottomNavigationView : BottomNavigationView? = null
+    private var today_recyclerView: RecyclerView?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_today_exercise)
 
         var intent = intent
-        var exerciseName = intent.getStringArrayExtra("exerciseName")
+        var exerciseNames = intent.getStringArrayListExtra("exerciseName")
 
+        today_recyclerView = findViewById(R.id.today_recyclerView)
+        today_recyclerView!!.layoutManager = LinearLayoutManager(this)
+        var adapter = exerciseNames?.let { ExerciseAdapter(it) }
+        today_recyclerView!!.adapter = adapter
 
+//        add = findViewById(R.id.add)
+//        save = findViewById(R.id.save)
+//
+//
+//        add!!.setOnClickListener{
+//            Toast.makeText(applicationContext, "Exercise Added", Toast.LENGTH_SHORT).show()
+//        }
+//
+//        save!!.setOnClickListener{
+//            Toast.makeText(applicationContext, "Exercise Saved", Toast.LENGTH_SHORT).show()
+//        }
 
-
-
-        add = findViewById(R.id.add)
-        save = findViewById(R.id.save)
-
-
-        add!!.setOnClickListener{
-            Toast.makeText(applicationContext, "Exercise Added", Toast.LENGTH_SHORT).show()
-        }
-
-        save!!.setOnClickListener{
-            Toast.makeText(applicationContext, "Exercise Saved", Toast.LENGTH_SHORT).show()
-        }
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNavigationView!!.selectedItemId = R.id.action_workout
-        bottomNavigationView!!.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+//        bottomNavigationView = findViewById(R.id.bottom_navigation)
+//        bottomNavigationView!!.selectedItemId = R.id.action_workout
+//        bottomNavigationView!!.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
